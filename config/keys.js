d569c7c65584554b2c76598019c4998bc62fc0ae
Yudong <1336839358@qq.com>
2020-09-17 15:53:54 +0800

    新增本地连接

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-17 10:36:53
+ * @LastEditTime: 2020-09-17 12:51:22
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \my-koa-project\config\keys.js
+ */
 module.exports = {
   mongoURI:
-    "mongodb+srv://root:root123@mycluster.arbsi.mongodb.net/sample_mflix?retryWrites=true&w=majority",
+    // "mongodb+srv://root:root123@mycluster.arbsi.mongodb.net/sample_mflix?retryWrites=true&w=majority",
+    "mongodb://localhost/koaapp",
 };
